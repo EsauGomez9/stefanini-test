d9357aadf1f8c58e8cb44e18afbbbb0e5aa9f622
Esaú Gómez <cursosesaugomez@gmail.com>
2022-11-09 22:38:04 -0600

    feat(person/controller): main person endpoints

--- src/controllers/v1/person/index.ts
+++ src/controllers/v1/person/index.ts
@@ -3,6 +3,19 @@ import controller from './person.controller'
 
 const ROUTER = Router()
 
+// Get person list
 ROUTER.route('/').get(controller.getPersonList as RequestHandler)
+// Get a specific person
+ROUTER.route('/:personId').get(controller.getPersonById as RequestHandler)
+
+// Add person
+ROUTER.route('/').post(controller.addPerson as RequestHandler)
+// Update a specific person
+ROUTER.route('/:personId').put(controller.updatePerson as RequestHandler)
+// Delete a specific person
+ROUTER.route('/:personId').delete(controller.deletePerson as RequestHandler)
+
+// Upload CSV
+ROUTER.route('/import').delete(controller.importPersons as RequestHandler)
 
 export default ROUTER
--- src/controllers/v1/person/person.controller.ts
+++ src/controllers/v1/person/person.controller.ts
@@ -10,6 +10,61 @@ async function getPersonList (req: Request, res: Response): Promise<Response> {
   }
 }
 
+async function getPersonById (req: Request, res: Response): Promise<Response> {
+  try {
+    return res.send('person')
+  } catch (error: unknown) {
+    return res.status(500).json({
+      message: error
+    })
+  }
+}
+
+async function addPerson (req: Request, res: Response): Promise<Response> {
+  try {
+    return res.send('person added')
+  } catch (error: unknown) {
+    return res.status(500).json({
+      message: error
+    })
+  }
+}
+
+async function deletePerson (req: Request, res: Response): Promise<Response> {
+  try {
+    return res.send('person deleted')
+  } catch (error: unknown) {
+    return res.status(500).json({
+      message: error
+    })
+  }
+}
+
+async function updatePerson (req: Request, res: Response): Promise<Response> {
+  try {
+    return res.send('person updated')
+  } catch (error: unknown) {
+    return res.status(500).json({
+      message: error
+    })
+  }
+}
+
+async function importPersons (req: Request, res: Response): Promise<Response> {
+  try {
+    return res.send('persons has been imported')
+  } catch (error: unknown) {
+    return res.status(500).json({
+      message: error
+    })
+  }
+}
+
 export default {
-  getPersonList
+  getPersonList,
+  getPersonById,
+  addPerson,
+  deletePerson,
+  updatePerson,
+  importPersons
 }
